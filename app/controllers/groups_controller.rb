817a61522e47f0d2a45780f5a8a8bd78b306291b
森瀬 隆司 <55taac.tac@gmail.com>
2018-12-25 21:17:27 +0900

    add action in group-controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,26 @@
 class GroupsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    Group.create.group_paramus(:name)
+  end
+
+  def edit
+  end
+
+  def update
+    if user.Gropu.name == group.name
+      Group.update(group_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
+  end
 end
